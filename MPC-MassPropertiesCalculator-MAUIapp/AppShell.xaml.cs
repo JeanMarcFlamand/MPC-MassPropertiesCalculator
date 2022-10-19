51ded495290f53bd46ea999b7c148ee7f92a259b
Jean-Marc Flamand <7jtf@videotron.ca>
2022-10-19 08:46:13 -0400

    Add testview page  to learn navigation using ShellContent

--- MPC-MassPropertiesCalculator-MAUIapp/AppShell.xaml.cs
+++ MPC-MassPropertiesCalculator-MAUIapp/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace MPC_MassPropertiesCalculator_MAUIapp
+﻿using MPC_MassPropertiesCalculator_MAUIapp.Views;
+
+namespace MPC_MassPropertiesCalculator_MAUIapp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(TestView),typeof(TestView));
+            
         }
     }
 }
--- MPC-MassPropertiesCalculator-MAUIapp/MauiProgram.cs
+++ MPC-MassPropertiesCalculator-MAUIapp/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿namespace MPC_MassPropertiesCalculator_MAUIapp
+﻿using MPC_MassPropertiesCalculator_MAUIapp.ViewModels;
+using MPC_MassPropertiesCalculator_MAUIapp.Views;
+
+namespace MPC_MassPropertiesCalculator_MAUIapp
 {
     public static class MauiProgram
     {
@@ -12,6 +15,9 @@
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            builder.Services.AddSingleton<TestView>();
+            builder.Services.AddSingleton<TestViewModel>();
 
             return builder.Build();
         }
