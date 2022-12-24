33b0666c239198e604bbdcc7cef4dea652ebaa64
Jean-Marc Flamand <7jtf@videotron.ca>
2022-12-24 09:13:30 -0500

    Added New splash screen Add DataGrid view page in wip add aboutme page in wip

--- MPC-MassPropertiesCalculator-MAUIapp/MauiProgram.cs
+++ MPC-MassPropertiesCalculator-MAUIapp/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using MPC_MassPropertiesCalculator_MAUIapp.ViewModels;
 using MPC_MassPropertiesCalculator_MAUIapp.Views;
 using MPCFilePickerMauiLibrary;
+using CommunityToolkit.Maui;
 
 namespace MPC_MassPropertiesCalculator_MAUIapp
 {
@@ -9,30 +10,22 @@ namespace MPC_MassPropertiesCalculator_MAUIapp
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
 
             builder.Services.AddSingleton<MPCDataGridView>();
-
             builder.Services.AddSingleton<TestView>();
             builder.Services.AddSingleton<TestViewModel>();
             builder.Services.AddSingleton<AboutmeView>();
             builder.Services.AddSingleton<AboutmeViewModel>();
-
+            //builder.Services.AddSingleton<GetMPCDemoFullFilePath>();
             //From Nuget package https://www.nuget.org/packages/MPCFilePickerMauiLibrary
             builder.Services.AddSingleton<PickTxtFile>();
             builder.Services.AddSingleton<PickFile>();
-
-
-
             return builder.Build();
         }
-        
     }
-    
 }
--- MPC-MassPropertiesCalculator-MAUIapp/Views/MPCDataGridView.xaml.cs
+++ MPC-MassPropertiesCalculator-MAUIapp/Views/MPCDataGridView.xaml.cs
@@ -1,4 +1,7 @@
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using MPCFilePickerMauiLibrary;
+
+
 
 namespace MPC_MassPropertiesCalculator_MAUIapp.Views;
 
@@ -26,12 +29,73 @@ public partial class MPCDataGridView : ContentPage
 
     }
 
+    private void Demo1_ClickedAsyn(object sender, EventArgs e)
+    {
+
+        var filename = "BOM MasspropCalc " + Demo1.Text;
+        OpenDemoFile(filename);
+    }
+
+    private void Demo2_Clicked(object sender, EventArgs e)
+    {
+        var filename = "BOM MasspropCalc " + Demo2.Text;
+        OpenDemoFile(filename);
+    }
+
+    private void Demo3_Clicked(object sender, EventArgs e)
+    {
+        var filename = "BOM MasspropCalc " + Demo3.Text;
+        OpenDemoFile(filename);
+    }
+
+    private void Demo4_Clicked(object sender, EventArgs e)
+    {
+        var filename = "BOM MasspropCalc " + Demo4.Text;
+        OpenDemoFile(filename);
+    }
+
+    private void Demo5_Clicked(object sender, EventArgs e)
+    {
+        var filename = "BOM MasspropCalc " + Demo5.Text;
+        OpenDemoFile(filename);
+    }
+
+    private void Demo6_Clicked(object sender, EventArgs e)
+    {
+        var filename = "BOM MasspropCalc " + Demo6.Text;
+        OpenDemoFile(filename);
+    }
+
+    public async void OpenDemoFile(string fileName)
+    {
+        char s = Path.DirectorySeparatorChar;
+        string mpcCFile = await LoadMauiAssetStream($"{Constants.ScenariosDataforTestingDirectory}{s}{fileName}");
+
+        FileOutput.Text = mpcCFile;
+        PickFileExpnd.IsExpanded = false;
+
+        await DisplayAlert("ReadAllText", $"Succesfull operation", "OK").ConfigureAwait(false);
+
+    }
+
     //This method returns a read-only Stream representing the file contents. 
     public async Task<string> ReadTextFile(string filePath)
     {
-        using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(filePath);
-        using StreamReader reader = new StreamReader(fileStream);
+        using var fileStream = await FileSystem.OpenAppPackageFileAsync(filePath );
+        using var reader = new StreamReader(fileStream);
 
         return await reader.ReadToEndAsync();
     }
+
+    
+
+    async Task<string> LoadMauiAssetStream(string filePath)
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync(filePath);
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        return contents;
+    }
+
+   
 }
